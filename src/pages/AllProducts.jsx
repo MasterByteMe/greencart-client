7f8320bf1b567e8ba85371bfcb01eee98edd3a62
Mark Anthony Ruiz <mark.devz.08@gmail.com>
2025-10-14 23:16:59 +0800

    feat: add AllProduct page
    - Created AllProduct page component
    - Added useState for filteredProducts
    - Added useEffect to display all products on page load
    - Implemented filter and map to render products from dummyData
    - Imported ProductCard component
    - Applied TailwindCSS styles for UI

--- src/pages/AllProducts.jsx
+++ src/pages/AllProducts.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useAppContext } from '../context/AppContext'
+import ProductCard from '../components/ProductCard'
 
 const AllProducts = () => {
 
-    const { products } = useAppContext;
+    const { products, searchQuery } = useAppContext();
+    const [filteredProducts, setFilterProducts] = useState([]);
+
+
+    useEffect(() => {
+
+        if (searchQuery.length > 0) {
+            setFilterProducts(products.filter(
+                product => product.name.toLowerCase().includes(searchQuery.toLowerCase())
+            ))
+        } else {
+            setFilterProducts(products)
+        }
+
+    }, [products, searchQuery]);
 
     return (
         <div className="mt-16 flex flex-col">
@@ -12,8 +27,11 @@ const AllProducts = () => {
                 <div className="w-16 h-0.5 bg-primary rounded-full"></div>
             </div>
 
-            <div>
-
+            <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6">
+                {filteredProducts.filter((product) => product.inStock).map((product, index) => (
+                    <ProductCard key={index} product={product} />
+                ))
+                }
             </div>
         </div>
     )
