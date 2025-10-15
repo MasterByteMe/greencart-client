2fd14108937fd3147ad0abdee880673e97fab66d
Mark Anthony Ruiz <mark.devz.08@gmail.com>
2025-10-16 05:38:47 +0800

    remove unused count useState

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,10 +4,8 @@ import { useAppContext } from "../context/AppContext";
 
 
 const ProductCard = ({ product }) => {
-    const [count, setCount] = React.useState(0);
+
     const { currency, addToCart, removeFromCart, cartItems, navigate } = useAppContext();
-
-
 
     return product && (
         <div onClick={() => { navigate(`/products/${product.category.toLowerCase()}/${product._id}`); scrollTo(0, 0) }} className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
