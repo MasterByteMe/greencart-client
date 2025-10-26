f4ddac36e641ba2256eaedd7c904945cf03256d2
Mark Anthony Ruiz <mark.devz.08@gmail.com>
2025-10-26 23:57:04 +0800

    feat: implement product submission handler with image upload and form data validation

--- src/pages/seller/AddProduct.jsx
+++ src/pages/seller/AddProduct.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import { assets } from "../../assets/assets";
 import { categories } from '../../assets/assets';
+import { useAppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
+
+
 
 const AddProduct = () => {
 
@@ -11,9 +15,43 @@ const AddProduct = () => {
     const [price, setPrice] = useState('');
     const [offerPrice, setOfferPrice] = useState('');
 
+    const { axios } = useAppContext();
 
     const onSubmitHandler = async (e) => {
-        e.preventDefault();
+        try {
+            e.preventDefault();
+            const productData = {
+                name,
+                description: description.split('\n'),
+                category,
+                price,
+                offerPrice
+            }
+
+            const formData = new FormData();
+            formData.append('productData', JSON.stringify(productData));
+            for (let i = 0; i < files.length; i++) {
+                formData.append('images', files[i]);
+            }
+
+            const { data } = await axios.post('/api/product/add', formData)
+
+            if (data) {
+                toast.success(data.message);
+                setName('');
+                setDescription('');
+                setCategory('');
+                setPrice('');
+                setOfferPrice('');
+                setFiles([]);
+            } else {
+                toast.error(data.message);
+            }
+
+        } catch (error) {
+            toast.error(error.message);
+        }
+
     }
 
 
