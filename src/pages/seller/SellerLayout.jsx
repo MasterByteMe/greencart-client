bb1758e47545cd4a78c1e2ad731eb090fc33ace8
Mark Anthony Ruiz <mark.devz.08@gmail.com>
2025-10-26 23:55:46 +0800

    enhance: improve seller logout function with axios request handling and toast notifications

--- src/pages/seller/SellerLayout.jsx
+++ src/pages/seller/SellerLayout.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { assets } from "../../assets/assets";
 import { useAppContext } from "../../context/AppContext";
 import { Link, NavLink, Outlet } from "react-router-dom";
-
+import toast from "react-hot-toast";
 
 const SellerLayout = () => {
 
-    const { setIsSeller } = useAppContext();
+    const { axios, navigate } = useAppContext();
 
     const sidebarLinks = [
         { name: "Add Product", path: "/seller", icon: assets.add_icon },
@@ -16,7 +16,17 @@ const SellerLayout = () => {
 
     // function to logout seller
     const logout = async () => {
-        setIsSeller(false);
+        try {
+            const { data } = await axios.get('api/seller/logout');
+            if (data.success) {
+                toast.success(data.message);
+                navigate('/');
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
     };
 
     return (
