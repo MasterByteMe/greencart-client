6ed77abbed86b238753d0d7a4e31d3d4bde296fb
Mark Anthony Ruiz <mark.devz.08@gmail.com>
2025-10-27 23:09:57 +0800

    feat: enhance auth form submission to show success toast for login or registration

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
 import { useAppContext } from '../context/AppContext'
+import toast from "react-hot-toast";
+
 
 const Login = () => {
 
-    const { setShowUserLogin, setUser } = useAppContext();
+    const { setShowUserLogin, setUser, axios, navigate } = useAppContext();
 
     const [state, setState] = React.useState("login");
     const [name, setName] = React.useState("");
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
 
+
+
     const onSubmitHandler = async (e) => {
-        e.preventDefault();
-        setUser({
-            email: "test@gmail.com",
-            password: "test@123"
-        })
-        setShowUserLogin(false);
+        try {
+            e.preventDefault();
+
+            const { data } = await axios.post(`api/user/${state}`, {
+                name, email, password
+            });
+            if (data.success) {
+                toast.success(state === 'login' ? 'Login successful!' : 'Registration successful!');
+                navigate('/')
+                setUser(data.user)
+                setShowUserLogin(false);
+            } else {
+                toast.error(data.message);
+
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
     }
 
 
